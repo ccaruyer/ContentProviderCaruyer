25c6eb1b0ddcf41ce512473ac6e1a5153a1214ec
Caruyer_Claire <claire.caruyer@gmail.com>
2023-06-28 14:44:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/contentprovidercaruyer/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.contentprovidercaruyer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
